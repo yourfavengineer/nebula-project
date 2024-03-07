642cbf2ffd6fb54cbfe99e279d63a8217686069d
yourfavengineer <kendall8584@yahoo.com>
2024-03-07 12:16:32 -0800

    nebula innitialization modification for Developmet

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "nebula-s3bucket"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    key            = "backend.tf/terraform.tfstate"
+    dynamodb_table = "nebula.dynamodb"
     encrypt        = true
   }
   required_version = ">=0.13.0"
